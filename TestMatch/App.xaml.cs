df24d0f4a7c996ea714313764066b7cf7cc6e0e6
Admin <Admin@LEGION7>
2026-07-03 13:57:08 +1000

    Add project files.

---
+++ TestMatch/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TestMatch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
