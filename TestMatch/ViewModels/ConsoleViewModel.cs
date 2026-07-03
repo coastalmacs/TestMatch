1054a70b0b5e210e95516876757422eb029599a0
Greg <greg@37a.com.au>
2026-07-03 14:58:47 +1000

    First commit

---
+++ TestMatch/ViewModels/ConsoleViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace TestMatch.ViewModels
+{
+
+    using System;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel; // Added for INotifyPropertyChanged
+    using System.Runtime.CompilerServices; // Added for CallerMemberName
+
+    public class ConsoleViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> LogOutput { get; set; } = new ObservableCollection<string>();
+
+        public void AddLogLine(string message)
+        {
+            // Ensure UI updates from background threads safely
+            System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            {
+                LogOutput.Add($"[{DateTime.Now:HH:mm:ss}] {message}");
+            });
+        }
+
+        // --- Fixes the compiler error ---
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+}
--- TestMatch/Views/MainWindow.xaml.cs
+++ TestMatch/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,5 +22,14 @@ namespace TestMatch
         {
             InitializeComponent();
         }
+
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
+
+
 }
